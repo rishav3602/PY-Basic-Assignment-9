9032c5d657944e2015ac7895df09de4fb512e0f8
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-03-10 19:50:56 +0530

    Add files via upload

---
+++ solutions.py
@@ -0,0 +1,57 @@
+## Questions..
+
+"""
+1. To what does a relative path refer?
+2. What does an absolute path start with your operating system?
+3. What do the functions os.getcwd() and os.chdir() do?
+4. What are the . and .. folders?
+5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+6. What are the three “mode” arguments that can be passed to the open() function?
+7. What happens if an existing file is opened in write mode?
+8. How do you tell the difference between read() and readlines()?
+9. What data structure does a shelf value resemble?
+
+
+"""
+
+## Answers...
+
+"""
+1. A relative path refers to the location of a file or directory relative to the current working directory.
+
+-------------------------------------------------------------------------------------------
+
+2. An absolute path starts with the root directory of the operating system and specifies the full path to a file or directory.
+
+-------------------------------------------------------------------------------------------
+
+3. The function os.getcwd() returns the current working directory, while os.chdir() changes the current working directory to the specified path.
+
+-------------------------------------------------------------------------------------------
+
+4. The "." folder represents the current directory, while ".." represents the parent directory.
+
+-------------------------------------------------------------------------------------------
+
+5. "C:\bacon\eggs" is the directory name, while "spam.txt" is the base name.
+
+-------------------------------------------------------------------------------------------
+
+6. The three "mode" arguments that can be passed to the open() function are "r" for read mode, "w" for write mode, and "a" for append mode.
+
+-------------------------------------------------------------------------------------------
+
+7. If an existing file is opened in write mode, the file's existing content will be truncated and replaced with the new data.
+
+-------------------------------------------------------------------------------------------
+
+8. The read() function returns the entire contents of a file as a single string, while readlines() returns a list of strings, where each string represents a line of the file.
+
+-------------------------------------------------------------------------------------------
+
+9. A shelf value in Python resembles a dictionary, where the keys and values are stored on disk instead of in memory.
+
+-------------------------------------------------------------------------------------------
+
+
+"""
